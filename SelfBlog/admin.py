7c3d0eef208dddd54638d1c6da0ba479f8f178ef
baron <baron@qq.com>
2019-07-15 22:14:01 +0800

    同步数据库

--- SelfBlog/admin.py
+++ SelfBlog/admin.py
@@ -3,7 +3,8 @@ from SelfBlog.models import *
 
 # Register your models here.
 
-admin.site.register(admin)
+
+
 admin.site.register(Article)
 admin.site.register(Types)
 admin.site.register(Message)
