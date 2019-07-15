155bc3e022d95913534fc49e7ffba9a7796c83f3
baron <baron@qq.com>
2019-07-15 22:04:32 +0800

    建立模型

--- SelfBlog/admin.py
+++ SelfBlog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from SelfBlog.models import *
 
 # Register your models here.
+
+admin.site.register(admin)
+admin.site.register(Article)
+admin.site.register(Types)
+admin.site.register(Message)
--- SelfBlog/models.py
+++ SelfBlog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+# from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
+
+class Article(models.Model):
+    title=models.CharField(max_length=64,null=False,verbose_name='标题')
+    content=RichTextField(verbose_name='正文')
+    images=models.ImageField(upload_to='images')
+    read_count=models.IntegerField(default=0,verbose_name='阅读数')
+    comment_count=models.IntegerField(default=0,verbose_name='评论数')
+    times=models.DateTimeField(auto_now_add=True)
+    types=models.ForeignKey('Types')
+    def __str__(self):
+        return self.title
+
+class Types(models.Model):
+    types=models.CharField(max_length=32)
+    def __str__(self):
+        return self.types
+
+class Message(models.Model):
+    message=RichTextField(verbose_name='评论')
+    article=models.ForeignKey('Article')
+    times=models.DateTimeField(auto_now_add=True,verbose_name='评论时间')
+    def __str__(self):
+        return self.message
